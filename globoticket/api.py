388950bf4e235e680eef8f6b261fb12edd4dbe11
jacheampong <jacheampong@gmail.com>
2024-06-27 11:13:16 -0400

    Initial commit

---
+++ globoticket/api.py
@@ -0,0 +1,16 @@
+"""
+api.py
+---
+
+REST API for Globoticket event database
+"""
+
+from pathlib import Path
+
+from fastapi import FastAPI
+from starlette.staticfiles import StaticFiles
+
+app = FastAPI()
+
+PROJECT_ROOT = Path(__file__).parent.parent
+app.mount("/", StaticFiles(directory=PROJECT_ROOT / "static", html=True))
